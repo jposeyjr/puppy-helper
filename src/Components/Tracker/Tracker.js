d27b915d96849a85a35f20ca07345bf40d9137a4
James Posey <jamesrposeyjr@gmail.com>
2020-12-15 08:28:04 -0600

    skelton of tracker in place

--- src/Components/Tracker/Tracker.js
+++ src/Components/Tracker/Tracker.js
@@ -1,7 +1,56 @@
 import React from 'react';
-
+import useStyles from './styles';
+import { Paper, TextField, Button, Typography } from '@material-ui/core';
+import Header from '../Header/Header';
 const Tracker = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.tracker}>
+      <Header />
+      <Paper elevation={3} className={classes.paper}>
+        <Typography variant='h3'>Potty Tracker</Typography>
+        <Typography variant='h6'>Activity Start</Typography>
+        <div className={classes.activity}>
+          <Button
+            variant={'contained'}
+            size='large'
+            color={'primary'}
+            className={classes.margin}
+          >
+            Food
+          </Button>
+          <Button
+            variant={'contained'}
+            size='large'
+            color={'primary'}
+            className={classes.margin}
+          >
+            Water
+          </Button>
+          <Button
+            variant={'contained'}
+            size='large'
+            color={'primary'}
+            className={classes.margin}
+          >
+            Play
+          </Button>
+          <Button
+            variant={'contained'}
+            size='large'
+            color={'primary'}
+            className={classes.margin}
+          >
+            Nap
+          </Button>
+        </div>
+        <Typography variant='h6'>Potty Achieved</Typography>
+        <Button variant={'contained'} size='large' color={'secondary'}>
+          Stop Timer
+        </Button>
+      </Paper>
+    </div>
+  );
 };
 
 export default Tracker;
