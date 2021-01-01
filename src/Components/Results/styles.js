8dea2c41fc72d1391ce42a81e7b2285031aa9f32
James Posey <jamesrposeyjr@gmail.com>
2021-01-01 09:48:53 -0600

    added charts and selector together for better visual appeal

--- src/Components/Results/styles.js
+++ src/Components/Results/styles.js
@@ -1,8 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  box: {
-    width: 500,
-    height: 300,
+  formControl: {
+    minWidth: 200,
+    background: '#FFFFFF',
+    color: '#000000',
+    borderRadius: 12,
+  },
+  select: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(0.5),
+  },
+  label: {
+    margin: theme.spacing(1),
   },
 }));
